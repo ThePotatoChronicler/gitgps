0dc0038668348cd4ff3e78841e5767f53078059a
Potato Chronicler <thepotatochronicler@gmail.com>
2025-05-15 21:01:06 +0200

    feat: replace vscode.git with simple-git

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,24 @@
 import * as vscode from 'vscode';
-import { type Remote, type GitExtension, type Repository, type Change, Status } from './vendored/git';
 import { Configuration } from './config';
 import { oneLine } from 'common-tags';
 import { assertUnreachable } from './util';
+import simpleGit, { RemoteWithRefs, SimpleGit } from 'simple-git';
+import { dirname, relative } from 'node:path';
+import gitUrlParse from 'git-url-parse';
 
 function getExtConfig(scope?: Parameters<typeof vscode.workspace.getConfiguration>[1]) {
 	return vscode.workspace.getConfiguration("gitgps", scope);
 }
 
-function getRemote(repository: Repository): Remote | null {
-	const remotes = repository.state.remotes;
+function stripBranchFromRemote(remote: string): string {
+	return remote.match(/(.+?)\//)?.[1] ?? remote;
+}
 
-	const headRemote = repository.state.HEAD?.upstream?.remote;
-	if (headRemote !== undefined) {
+async function getRemote(git: SimpleGit): Promise<RemoteWithRefs | null> {
+	const remotes = await git.getRemotes(true);
+
+	const headRemote = stripBranchFromRemote(await git.revparse(["--abbrev-ref", "@{upstream}"]));
+	if (headRemote.length > 0) {
 		const remote = remotes.find(r => r.name === headRemote);
 		if (remote !== undefined) {
 			return remote;
@@ -37,34 +43,12 @@ function replaceVariablesCustomUrl(customUrl: string, variables: { [v: string]: 
 	return customUrl.replace(/(?<!\\){([^{}]*?)(?<!\\)}/g, (_match, varKey) => variables[varKey] ?? "");
 }
 
-async function getGitConfig(repository: Repository, key: string): Promise<string> {
-	const localConfig = await repository.getConfig(key);
-	if (localConfig.length > 0) {
-		return localConfig;
-	}
-
-	return await repository.getGlobalConfig(key);
-}
-
-function getFileChanges(repository: Repository, uri: vscode.Uri): Change[] {
-	return repository.state.workingTreeChanges.filter(c => c.uri.toString() === uri.toString());
+async function getGitConfig(git: SimpleGit, key: string): Promise<string | null> {
+	return (await git.getConfig(key)).value;
 }
 
 function refineURL(url: string): string {
-	return url
-		// WARN: This breaks if by any chance the URL contains a port,
-		//       but I've never seen any git with a port
-		.replace(/^(https?:\/\/)?(?:(?:.+?)@)?(?:([^:]*?)|(.*?):(.*?))(?:.git)?$/,
-			(_match, schema, maybe_uri, maybe_domain, maybe_path) => 
-				(
-					schema ? schema : "https://"
-				)
-				+ (
-					maybe_uri
-					? maybe_uri
-					: `${maybe_domain}/${maybe_path}`
-				)
-			);
+	return gitUrlParse(url).toString("https").replace(/\.git$/, "");
 }
 
 function formatLine(
@@ -96,65 +80,66 @@ async function openCurrentLine(
 			permalink?: boolean
 		} = {}
 	) {
-	const git = vscode.extensions.getExtension<GitExtension>("vscode.git")?.exports.getAPI(1);
-	if (git === undefined) {
-		vscode.window.showErrorMessage("Cannot get extension vscode.git");
-		return;
-	}
-
 	const editor = vscode.window.activeTextEditor;
 	if (editor === undefined) {
 		vscode.window.showErrorMessage("No active text editor");
 		return;
 	}
 
-	const repository = git.getRepository(editor.document.uri);
+	const git = simpleGit({
+		baseDir: dirname(editor.document.fileName),
+	});
 
-	if (repository === null) {
+	if (!await git.checkIsRepo()) {
 		vscode.window.showErrorMessage("Currently opened file is not in a git repository");
 		return;
 	}
+
+	const toplevel = await git.revparse("--show-toplevel");
 
 	const useCustomUrl = getExtConfig().get<boolean>("customURL.enabled")!;
 
 	const lineStart = 1 + editor.selection.start.line;
 	const lineEnd = 1 + editor.selection.end.line;
-	const filepath = vscode.workspace.asRelativePath(editor.document.uri);
+	const filepath = relative(toplevel, editor.document.fileName);
+
+	const headCommit = await git.revparse("HEAD");
+	const headSymbolic = await git.revparse(["--abbrev-ref", "HEAD"]);
+
+	const ref = permalink ? headCommit : headSymbolic;
 
 	if (useCustomUrl) {
 		const customUrl = getExtConfig().get<string>("customURL.url")!;
 		vscode.env.openExternal(vscode.Uri.parse(replaceVariablesCustomUrl(customUrl, {
-			username: (await getGitConfig(repository, "user.name")).replaceAll(" ", ""),
-			ref: permalink ? repository.state.HEAD?.commit : repository.state.HEAD?.name,
+			username: (await getGitConfig(git, "user.name") ?? "").replaceAll(" ", ""),
+			ref,
 			lineGithub: formatLine({ lineStart, lineEnd, format: "github" }),
 			lineBitbucket: formatLine({ lineStart, lineEnd, format: "bitbucket" }),
 			filepath,
 			folderName: vscode.workspace.getWorkspaceFolder(editor.document.uri)?.name,
 		})));
 	} else {
-		const remote = getRemote(repository);
+		const remote = await getRemote(git);
 		if (remote === null) {
 			vscode.window.showErrorMessage("Current git repository has no remotes");
 			return;
 		}
 
-		const changes = getFileChanges(repository, editor.document.uri);
+		const changes = await git.status();
 
-		if (changes.length > 0) {
-			const change = changes[0];
-			if (change.status === Status.UNTRACKED) {
-				vscode.window.showErrorMessage("Current file is untracked, and has no remote URL");
-				return;
-			}
-			if ([Status.MODIFIED, Status.BOTH_MODIFIED].includes(change.status)) {
-				vscode.window.showWarningMessage(oneLine`
-					Current file is modified, line number will most likely be wrong
-					(we do not support partially modified files)
-				`); // TODO: Maybe add support?
-			}
+		if (changes.not_added.includes(filepath) || changes.created.includes(filepath)) {
+			vscode.window.showErrorMessage("Current file is untracked, and has no remote URL");
+			return;
 		}
 
-		const remoteUrl = remote.fetchUrl ?? remote.pushUrl;
+		if (changes.modified.includes(filepath)) {
+			vscode.window.showWarningMessage(oneLine`
+				Current file is modified, line number will most likely be wrong
+				(we do not support partially modified files)
+			`); // TODO: Maybe add support?
+		}
+
+		const remoteUrl = remote.refs.fetch ?? remote.refs.push;
 		if (remoteUrl === undefined) {
 			vscode.window.showErrorMessage("No remote URL");
 			return;
@@ -166,23 +151,15 @@ async function openCurrentLine(
 
 		const line = formatLine({ lineStart, lineEnd, format: isBitbucket ? "bitbucket" : "github" });
 
-		const ref =
-			permalink
-			? repository.state.HEAD?.commit
-			: (repository.state.HEAD?.name ?? repository.state.HEAD?.commit);
-		
 		const uri =
 			isBitbucket
 			? vscode.Uri.joinPath(
 					baseUri,
 					"src",
-					ref ?? "",
+					headCommit ?? "",
 					filepath,
 				).with({
-					query:
-						repository.state.HEAD?.name
-						? `at=${repository.state.HEAD.name}`
-						: "",
+					query: `at=${headSymbolic}`,
 					fragment: `lines-${line}`,
 				})
 			: vscode.Uri.joinPath(
